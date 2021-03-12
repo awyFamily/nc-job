b444bbdeed02e2b6a4c5cba6f3efcba1a11930d0
yehongwei <931163482@qq.com>
2021-03-12 09:57:39 +0800

    完善相关类

--- nc-job-core/src/main/java/com/yhw/nc/job/core/config/JobConfig.java
+++ nc-job-core/src/main/java/com/yhw/nc/job/core/config/JobConfig.java
@@ -1,8 +1,14 @@
 package com.yhw.nc.job.core.config;
 
+import com.yhw.nc.job.api.constants.CommonConstant;
+import com.yhw.nc.job.api.constants.JobCommonConstant;
 import com.yhw.nc.job.api.feign.IJobAdminCallback;
 import lombok.Getter;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -11,6 +17,9 @@ import javax.annotation.Resource;
  * @author yhw
  * @date 2019-09-30
  */
+@Configuration
+@ComponentScan(basePackages = CommonConstant.GROUP_ID)
+@EnableFeignClients(basePackages = {JobCommonConstant.ADMIN_FEIGN_REMOTE_PATH} )
 @Component
 public class JobConfig implements InitializingBean {
 
